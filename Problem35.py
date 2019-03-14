f1f13920273324d5a6bd87e8d0082c427786f91a
emsinko <44008559+emsinko@users.noreply.github.com>
2019-03-14 13:19:49 +0100

    Create Problem35.py

---
+++ Problem35.py
@@ -0,0 +1,23 @@
+## PROBLEM 35
+
+"""
+The number, 197, is called a circular prime because all rotations of the digits: 
+    197, 971, and 719, are themselves prime.
+There are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, and 97.
+How many circular primes are there below one million?
+"""
+    
+odd = [1,3,5,7,9]  #np.arange(1,9+1,2)
+circular = [2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, 97]
+
+import itertools
+perm = list(itertools.permutations([1,2,3]))
+1:14:2
+x[2]
+x[2]
+x[[0,2,1]]
+print(perm)
+x = "194"
+np.arraalen(x)
+(itertools.permutations([1,2,3]))
+list(itertools.permutations(x))
